69434400f1d98b6e8d5be72beea57948d36ec7e0
Geraldo Fada <geraldofada.neto@gmail.com>
2018-10-12 18:29:56 -0300

    Proteja sua senha solution

--- ad-hoc/proteja_senha_2287.py
+++ ad-hoc/proteja_senha_2287.py
@@ -1,10 +1,13 @@
 times = int(input())
 
+def frequency(lst):
+    return max(lst, key=lst.count)
+
 test = 1
 while(times != 0):
     print("Teste {}".format(test))
 
-    password_numbers = []
+    password_numbers = [[], [], [], [], [], []]
     for i in range(times):
         password = {}
         list_input = list(input().split())
@@ -14,13 +17,24 @@ while(times != 0):
         password['D'] = [list_input[6], list_input[7]]
         password['E'] = [list_input[8], list_input[9]]
 
-        pass_temp = []
+        j = 0
         for letter in list_input[10:]:
-            pass_temp.append(password[letter])
-        password_numbers.append(pass_temp)
+            for num in password[letter]:
+                password_numbers[j].append(num)
+            j += 1
+    
+    final = []
+    for passw in password_numbers:
+        final.append(frequency(passw))
+    
+    print("{} {} {} {} {} {} \n".format(
+        final[0],
+        final[1],
+        final[2],
+        final[3],
+        final[4],
+        final[5],
+    ))
 
-        # sei lá né mano...
-        
-    print(password_numbers)
     test += 1
     times = int(input())
